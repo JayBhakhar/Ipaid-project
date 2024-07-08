11800727065e6017e7c1921d4e289b7007aa41e5
JayBhakhar <49015779+JayBhakhar@users.noreply.github.com>
2024-07-08 17:35:59 +0400

    Update __init__.py
    Remove Comment

--- ipaid/__init__.py
+++ ipaid/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -10,6 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Ip
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db) # this
+migrate = Migrate(app, db)
 
 from ipaid import routes
